662421d81c1effab57b9eeed125a972f75ba2498
ryzac <rzc044@gmail.com>
2024-09-13 21:38:17 -0500

    Initial commit - backend setup and connection with database.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express(); 
+
+app.post("/products", (req, res) => {
+
+})
+
+app.listen(5000, () => {
+    connectDB();
+    console.log('Server started at http://localhost:5000');
+});
+
